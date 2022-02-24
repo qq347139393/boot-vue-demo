8cc0b22c7885df495b969f9b09a827fe951138fc
jja1989 <347139393@qq.com>
2022-02-24 11:32:37 +0800

    1

---
+++ src/test/java/com/bootvuedemo/BootVueDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootvuedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootVueDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
